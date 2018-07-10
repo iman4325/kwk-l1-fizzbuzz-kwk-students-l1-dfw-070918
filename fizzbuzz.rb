b0aaf40c54d0ab6779efa2cad0a16e59168fef73
iman4325 <imantanzeem@hotmail.com>
2018-07-10 18:12:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizz_buzz(your_num)
   your_num = gets.chomp
   if your_num % 3 ==0 && your_num % 5 ==0
+    puts "Fizz Buzz"
+  else if your_num % 3 == 0
+    puts "Fizz"
+  else your_num % 5 ==0
+    puts "Buzz"
+end 
