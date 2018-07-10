9f4fd2c5d9f5455eef9b4b9d0817172399294048
iman4325 <imantanzeem@hotmail.com>
2018-07-10 18:25:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 def fizz_buzz
   int = gets.chomp
-  if int % 3 ==0 && int % 5 ==0
+  if int%3==0 && int%5==0
      puts "Fizz Buzz"
-  else if int % 3 == 0
+  elsif int % 3 == 0
      puts "Fizz"
-  else int % 5 ==0
+  else int%5==0
      puts "Buzz"
 end 
 end 
