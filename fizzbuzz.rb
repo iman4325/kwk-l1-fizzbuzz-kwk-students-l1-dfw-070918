dd1acd65e3828a33469c8b83383c0d0ccea0551f
iman4325 <imantanzeem@hotmail.com>
2018-07-10 18:31:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 def fizz_buzz
   your_num = 3 
   puts int
-  if your_num%3==0 && your_num%5==0
+  if your_num %3==0 && your_num %5==0
      puts "Fizz Buzz"
   elsif your_num % 3 == 0
      puts "Fizz"
-  else your_num%5==0
+  else your_num %5==0
      puts "Buzz"
 end 
 end 
